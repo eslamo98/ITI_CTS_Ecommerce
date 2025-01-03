06a1001a7d8e86be042337a511ca7da5dd366cf6
hager_hatem_ <hagerhatem24@gmail.com>
2025-01-03 22:08:02 +0200

    Add Index code and home.js/.css and related images to it

---
+++ Views/js/home.js
@@ -0,0 +1,26 @@
+//Timer 
+// Set the date we're counting down to
+const countDownDate = new Date("Jan 20, 2025 00:00:00").getTime();
+
+// Update the countdown every 1 second
+const x = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days.toString().padStart(2, '0');
+    document.getElementById("hours").innerHTML = hours.toString().padStart(2, '0');
+    document.getElementById("minutes").innerHTML = minutes.toString().padStart(2, '0');
+    document.getElementById("seconds").innerHTML = seconds.toString().padStart(2, '0');
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
+//********************************************************************************
