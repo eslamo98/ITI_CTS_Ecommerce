fae145409bf25d9c80cba8abf5f2c204ac17f227
Mohamed_Elgendy <mohamedelg266@gmail.com>
2025-01-07 14:42:54 +0200

    add role

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,6 +1,6 @@
 import { UsersRepo } from "../Repository/UsersRepo.js";
 import { User } from "../Models/User.js";
-
+import { Helpers } from "../Utils/Helpers.js";
 export class UsersController {
     signUp() {
         let name = document.getElementById("name").value;
@@ -10,13 +10,22 @@ export class UsersController {
         let address = document.getElementById("address").value;
         let email = document.getElementById("email").value;
         let password = document.getElementById("password").value;
+        let role = document.getElementById("role").value;
+   let roleId;
+        if(role=="user"){
+            roleId=1;
+        }
+        else if(role=="seller"){
+            roleId=2;
+        }
+        else roleId=3;
 
         if (UsersRepo.validateEmail(email) == "email is exist already") {
             alert("email is exist already");
         } else if (UsersRepo.validatePhone(phone) == "phone is exist already") {
             alert("phone is exist already");
         } else {
-            let user = new User(name, firstName, lastName, phone, email, 1, password, address, "./images/Users/avatar.png");
+            let user = new User(name, firstName, lastName, phone, email, roleId, password, address, "./images/Users/avatar.png");
             UsersRepo.saveUser(user);
             alert("user added successfully");
         }
